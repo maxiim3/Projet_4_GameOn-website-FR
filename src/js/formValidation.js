040d40baf524c35df9e14071c111c7b9b9dfbabf
Maxime Tamburrini <maximemickael.contact@gmail.com>
2022-07-17 15:16:48 +0200

    js oke

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -1,136 +1,136 @@
-const ageMinimum = 18;
-
-export const form = document.getElementById('form');
-export const firstname = document.getElementById('first');
-export const lastname = document.getElementById('last');
-export const email = document.getElementById('email');
-export const birthdate = document.getElementById('birthdate');
-export const quantity = document.getElementById('quantity');
-export const radios = [...document.querySelectorAll('#radio .checkbox-input')];
-export const checkboxCGU = document.querySelector('#CGU #checkbox1');
-export const feedback = document.getElementById('feedback');
-
-export async function formValidation() {
-   const firstnameValue = firstname.value.trim();
-   const lastnameValue = lastname.value.trim();
-   const emailValue = email.value.trim();
-   const birthdateStrictValue = birthdate.value.trim();
-   const birthdateDateValue = new Date(birthdateStrictValue);
-   const birthdayYear = birthdateDateValue.getUTCFullYear();
-   const quantityValue = quantity.value.trim();
-
-   let count = 0;
-   let total = 0;
-
-   const checkUserInput = () => {
-      total++;
-      if (firstnameValue === '') {
-         setErrorTo(firstname, 'Le prénom doit être renseigner');
-      } else if (firstnameValue.length < 2) {
-         setErrorTo(firstname, "Veuillez renseigner un prénom d'au moins deux lettres svp ");
-      } else {
-         setValidTo(firstname);
-         count++;
-      }
-      total++;
-      if (lastnameValue === '') {
-         setErrorTo(lastname, 'Le nom doit être renseigner');
-      } else if (lastnameValue.length < 2) {
-         setErrorTo(firstname, "Veuillez renseigner un nom d'au moins deux lettres svp ");
-      } else {
-         setValidTo(lastname);
-         count++;
-      }
-      total++;
-      if (emailValue === '') {
-         setErrorTo(email, "L'email doit être renseigner");
-      } else if (!isEmail(emailValue)) {
-         setErrorTo(email, 'Veuillez renseigner un email valid svp ');
-      } else {
-         setValidTo(email);
-         count++;
-      }
-      total++;
-      if (birthdateStrictValue === '') {
-         setErrorTo(birthdate, 'La date de naissance doit être renseigner');
-      } else if (!isAdult(birthdayYear)) {
-         setErrorTo(birthdate, 'Vous devez avoir 18ans pour participer');
-      } else {
-         setValidTo(birthdate);
-         count++;
-      }
-      total++;
-      if (quantityValue === '') {
-         setErrorTo(quantity, 'Vous devez renseigner nombre de tournois');
-      } else {
-         setValidTo(quantity);
-         count++;
-      }
-      if (radios.every(radio => !radio.checked)) {
-         setErrorTo(radios[0], 'Vous devez choisir un tournois');
-      } else {
-         setValidTo(radios[0]);
-         count++;
-      }
-      total++;
-      if (!checkboxCGU.checked) {
-         setErrorTo(checkboxCGU, "Vous devez accepter les conditions d'utilisation");
-      } else {
-         setValidTo(checkboxCGU);
-         count++;
-      }
-      total++;
-      return count === total;
-   };
-
-   checkUserInput();
-   if (checkUserInput()) {
-      form.dataset.formValid = true;
-      feedback.dataset.formValid = true;
-      return {
-         firstname: firstnameValue,
-         lastname: lastnameValue,
-         email: emailValue,
-         birthdateStrict: birthdateStrictValue,
-         birthdateDate: birthdateDateValue,
-         birthday: birthdayYear,
-         quantity: quantityValue,
-      };
-   } else {
-      form.dataset.formValid = false;
-      feedback.dataset.formValid = false;
-      console.error('Form is not valid');
-   }
-}
-
-// if error validation, set data-error to true to parent element and display error message
-export function setErrorTo(input, message) {
-   const formData = input.parentElement;
-   formData.dataset.error = message;
-   formData.dataset.errorVisible = true;
-}
-
-// if validation succeed,  set data-error to false to parent element
-export function setValidTo(input) {
-   const formData = input.parentElement;
-   formData.dataset.errorVisible = false;
-}
-
-// Check if email is valid using regex test
-export function isEmail(email) {
-   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-   );
-}
-
-// Check if the User is Adult
-export function isAdult(input) {
-   const actualDate = new Date();
-   const actualYear = actualDate.getUTCFullYear();
-
-   const minimumDate = new Date();
-   minimumDate.setFullYear(ageMinimum);
-   const minimumYear = minimumDate.getUTCFullYear();
-
-   return input < actualYear - minimumYear;
-}
+// const ageMinimum = 18;
+//
+// export const form = document.getElementById('form');
+// export const firstname = document.getElementById('first');
+// export const lastname = document.getElementById('last');
+// export const email = document.getElementById('email');
+// export const birthdate = document.getElementById('birthdate');
+// export const quantity = document.getElementById('quantity');
+// export const radios = [...document.querySelectorAll('#radio .checkbox-input')];
+// export const checkboxCGU = document.querySelector('#CGU #checkbox1');
+// export const feedback = document.getElementById('feedback');
+//
+// export async function formValidation() {
+//    const firstnameValue = firstname.value.trim();
+//    const lastnameValue = lastname.value.trim();
+//    const emailValue = email.value.trim();
+//    const birthdateStrictValue = birthdate.value.trim();
+//    const birthdateDateValue = new Date(birthdateStrictValue);
+//    const birthdayYear = birthdateDateValue.getUTCFullYear();
+//    const quantityValue = quantity.value.trim();
+//
+//    let count = 0;
+//    let total = 0;
+//
+//    const checkUserInput = () => {
+//       total++;
+//       if (firstnameValue === '') {
+//          setErrorTo(firstname, 'Le prénom doit être renseigner');
+//       } else if (firstnameValue.length < 2) {
+//          setErrorTo(firstname, "Veuillez renseigner un prénom d'au moins deux lettres svp ");
+//       } else {
+//          setValidTo(firstname);
+//          count++;
+//       }
+//       total++;
+//       if (lastnameValue === '') {
+//          setErrorTo(lastname, 'Le nom doit être renseigner');
+//       } else if (lastnameValue.length < 2) {
+//          setErrorTo(lastname, "Veuillez renseigner un nom d'au moins deux lettres svp ");
+//       } else {
+//          setValidTo(lastname);
+//          count++;
+//       }
+//       total++;
+//       if (emailValue === '') {
+//          setErrorTo(email, "L'email doit être renseigner");
+//       } else if (!isEmail(emailValue)) {
+//          setErrorTo(email, 'Veuillez renseigner un email valid svp ');
+//       } else {
+//          setValidTo(email);
+//          count++;
+//       }
+//       total++;
+//       if (birthdateStrictValue === '') {
+//          setErrorTo(birthdate, 'La date de naissance doit être renseigner');
+//       } else if (!isAdult(birthdayYear)) {
+//          setErrorTo(birthdate, 'Vous devez avoir 18ans pour participer');
+//       } else {
+//          setValidTo(birthdate);
+//          count++;
+//       }
+//       total++;
+//       if (quantityValue === '') {
+//          setErrorTo(quantity, 'Vous devez renseigner nombre de tournois');
+//       } else {
+//          setValidTo(quantity);
+//          count++;
+//       }
+//       if (radios.every(radio => !radio.checked)) {
+//          setErrorTo(radios[0], 'Vous devez choisir un tournois');
+//       } else {
+//          setValidTo(radios[0]);
+//          count++;
+//       }
+//       total++;
+//       if (!checkboxCGU.checked) {
+//          setErrorTo(checkboxCGU, "Vous devez accepter les conditions d'utilisation");
+//       } else {
+//          setValidTo(checkboxCGU);
+//          count++;
+//       }
+//       total++;
+//       return count === total;
+//    };
+//
+//    checkUserInput();
+//    if (checkUserInput()) {
+//       form.dataset.formValid = true;
+//       feedback.dataset.formValid = true;
+//       return {
+//          firstname: firstnameValue,
+//          lastname: lastnameValue,
+//          email: emailValue,
+//          birthdateStrict: birthdateStrictValue,
+//          birthdateDate: birthdateDateValue,
+//          birthday: birthdayYear,
+//          quantity: quantityValue,
+//       };
+//    } else {
+//       form.dataset.formValid = false;
+//       feedback.dataset.formValid = false;
+//       console.error('Form is not valid');
+//    }
+// }
+//
+// // if error validation, set data-error to true to parent element and display error message
+// export function setErrorTo(input, message) {
+//    const formData = input.parentElement;
+//    formData.dataset.error = message;
+//    formData.dataset.errorVisible = true;
+// }
+//
+// // if validation succeed,  set data-error to false to parent element
+// export function setValidTo(input) {
+//    const formData = input.parentElement;
+//    formData.dataset.errorVisible = false;
+// }
+//
+// // Check if email is valid using regex test
+// export function isEmail(email) {
+//    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+//       email
+//    );
+// }
+//
+// // Check if the User is Adult
+// export function isAdult(input) {
+//    const actualDate = new Date();
+//    const actualYear = actualDate.getUTCFullYear();
+//
+//    const minimumDate = new Date();
+//    minimumDate.setFullYear(ageMinimum);
+//    const minimumYear = minimumDate.getUTCFullYear();
+//
+//    return input < actualYear - minimumYear;
+// }
---
+++ starterOnly/modal.js
@@ -0,0 +1,250 @@
+// region Navigation
+const iconNavBar = document.getElementById('iconNavBar');
+// open toNav menu
+iconNavBar.addEventListener('click', editNav);
+
+// set topNav classes
+function editNav() {
+   const topNav = document.getElementById('myTopnav');
+   if (topNav.className === 'topnav') {
+      topNav.className += ' responsive';
+   } else {
+      topNav.className = 'topnav';
+   }
+}
+
+// endregion
+
+// region Modal Control
+const modalBg = document.querySelector('.bground');
+const modalBtn = [...document.querySelectorAll('.modal-btn')];
+const closeBtn = [...document.querySelectorAll('.close-modal')];
+// launch modal e[vent]
+modalBtn.forEach(btn => btn.addEventListener('click', launchModal));
+// close modal event
+closeBtn.forEach(btn => btn.addEventListener('click', closeModal));
+
+// launch modal form
+function launchModal() {
+   modalBg.style.display = 'block';
+}
+
+// close modal form
+function closeModal() {
+   modalBg.style.display = 'none';
+}
+
+// endregion
+
+//region Form Validation
+const btnSubmit = document.querySelector('.btn-submit');
+btnSubmit.addEventListener('click', validate);
+
+/**
+ * # Validate Form
+ * Envoie le formulaire si les saisies utilisateurs sont valides
+ * @param e
+ */
+function validate(e) {
+   e.preventDefault();
+   // Form Elements
+   const form = document.getElementById('form');
+   const feedback = document.getElementById('feedback');
+
+   try {
+      (function () {
+         if (checkUserInput()) {
+            form.dataset.formValid = 'true';
+            feedback.dataset.formValid = 'true';
+            printMsg('Formulaire envoyé avec succès!', 'green');
+         } else {
+            form.dataset.formValid = 'false';
+            feedback.dataset.formValid = 'false';
+            throw new Error("Le formulaire ne peut être envoyé car il n'est pas valide");
+         }
+      })();
+   } catch (e) {
+      console.warn(`... ${e.name} : ${e.message}`);
+   }
+}
+
+/**
+ * # checkUserInput
+ * Vérifie que les saisies utilisateurs sont valides
+ * @returns {boolean}
+ */
+function checkUserInput() {
+   //region DOM Form Elements
+   const firstname = document.getElementById('first');
+   const lastname = document.getElementById('last');
+   const email = document.getElementById('email');
+   const birthdate = document.getElementById('birthdate');
+   const quantity = document.getElementById('quantity');
+   const radios = [...document.querySelectorAll('#radios .checkbox-input')];
+   const checkboxCGU = document.querySelector('#checkboxes #checkbox1');
+
+   //endregion
+
+   printMsg('Checking Values....', 'tomato');
+
+   let validInputs = 0;
+   let requiredValid = 0;
+
+   //region Block de Conditions
+   // validation prenom
+   requiredValid++;
+   if (!isEmpty(firstname)) {
+      if (!minLength(firstname, 2)) {
+         setValidTo(firstname);
+         validInputs++;
+      }
+   }
+   // validation nom
+   requiredValid++;
+   if (!isEmpty(lastname)) {
+      if (!minLength(lastname, 2)) {
+         setValidTo(lastname);
+         validInputs++;
+      }
+   }
+   // validation mail
+   requiredValid++;
+   if (!isEmpty(email)) {
+      if (isEmail(email)) {
+         setValidTo(email);
+         validInputs++;
+      }
+   }
+   // validation date de naissance
+   requiredValid++;
+   if (!isEmpty(birthdate)) {
+      setValidTo(birthdate);
+      validInputs++;
+   }
+   // validation nombre de participation
+   requiredValid++;
+   if (!isEmpty(quantity)) {
+      setValidTo(quantity);
+      validInputs++;
+   }
+   // validation choix de la ville
+   requiredValid++;
+   if (!isSelected(radios)) {
+      setValidTo(radios.at(0));
+      validInputs++;
+   }
+   // validation CGU
+   requiredValid++;
+   if (!isChecked(checkboxCGU)) {
+      setValidTo(checkboxCGU);
+      validInputs++;
+   }
+   //endregion
+
+   console.log(`validInputs: ${validInputs}`);
+   console.log(`requiredValid: ${requiredValid}`);
+   return validInputs === requiredValid;
+}
+
+/**
+ * Fonction raccourcie pour afficher un message coloré dans la console
+ * @param msg
+ * @param color
+ */
+function printMsg(msg, color = 'black') {
+   console.log(`%c... ${msg} ...`, `color: ${color}; font-size: 15px`);
+}
+
+/**
+ * Vérifie que la valeur de l'input ne soit pas vide
+ * @param input
+ * @returns {boolean}
+ */
+function isEmpty(input) {
+   if (input.value.trim() === '') {
+      setErrorTo(input, `${input.ariaLabel} doit être renseigner`);
+      return true;
+   }
+   return false;
+}
+
+/**
+ * Vérifie si la longueur des noms ou prénom est valide
+ * @param input
+ * @param i
+ * @returns {boolean}
+ */
+function minLength(input, i) {
+   if (input.value.trim().length < i) {
+      setErrorTo(input, `${input.ariaLabel} doit faire au moins 2 lettres`);
+      return true;
+   }
+   return false;
+}
+
+/**
+ * Vérifie si le format de mail est valide
+ * @param email
+ * @returns {boolean}
+ */
+function isEmail(email) {
+   const regex =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   if (regex.test(email.value.trim())) {
+      return true;
+   } else {
+      setErrorTo(email, 'Veuillez renseigner un email valid svp');
+      return false;
+   }
+}
+
+/**
+ * Vérifie si une ville a été sélectionnée
+ * @param radios
+ * @returns {boolean}
+ */
+function isSelected(radios) {
+   if (radios.every(radio => !radio.checked)) {
+      setErrorTo(radios[0], 'Vous devez choisir un tournois');
+      return true;
+   }
+   return false;
+}
+
+/**
+ * Vérifie que les CGU ont bien été acceptées
+ * @param checkbox
+ * @returns {boolean}
+ */
+function isChecked(checkbox) {
+   if (!checkbox.checked) {
+      setErrorTo(checkbox, "Vous devez accepter les conditions d'utilisation");
+      return true;
+   }
+   return false;
+}
+
+/**
+ * Active les styles [data-error-visible] pour l'élément parent
+ *
+ * Affiche le message d'erreur
+ * @param input
+ * @param message
+ */
+function setErrorTo(input, message) {
+   const formData = input.parentElement;
+   formData.dataset.error = message;
+   formData.dataset.errorVisible = true;
+   // throw new Error(message);
+}
+
+/**
+ * Désactive les styles [data-error-visible] pour l'élément parent
+ * @param input
+ */
+function setValidTo(input) {
+   const formData = input.parentElement;
+   formData.dataset.errorVisible = false;
+}
+
+//endregion
